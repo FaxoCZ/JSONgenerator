a36f5e53c35d2439f440bd5f15f251c0ecafedfb
PesekAntonin <pesekantonin@sssvt.cz>
2026-04-02 18:34:41 +0200

    Přidejte soubory projektu.

---
+++ JSONgenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JSONgenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
